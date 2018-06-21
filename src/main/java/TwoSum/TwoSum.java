eea6c6752303f3c72327ad2eb3a6bc406f92c2ec
cheng.zhang <mr.cheng94@foxmail.com>
2018-06-21 11:29:01 +0800

    Initial commit

---
+++ src/main/java/TwoSum/TwoSum.java
@@ -0,0 +1,88 @@
+package TwoSum;
+
+/**
+ * solution:https://leetcode-cn.com/articles/two-sum/
+ *
+ * @author sailfish
+ * @create 2018-06-21-上午10:47
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific
+ * target. You may assume that each input would have exactly one solution, and you may not use the
+ * same element twice. Example:
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
+ */
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        int length = nums.length;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = i + 1; j < length; j++) {
+                int sum = 0;
+                sum = nums[i] + nums[j];
+
+                if (sum == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+    /**
+     * method2
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * methhod3
+     */
+    public static int[] twoSum3(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+//        int[] nums = {2, 7, 11, 15};
+        int[] nums = {3, 2, 4};
+//        int[] ints = twoSum(nums, 6);
+        int[] ints = twoSum2(nums, 6);
+        if (ints != null) {
+            for (int anInt : ints) {
+                System.out.println(anInt);
+            }
+        }
+
+    }
+}
